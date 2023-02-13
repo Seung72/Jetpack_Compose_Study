0348edfb0029552759e5a7eacd7c42047fe5130e
seungcheol <leesc5724@naver.com>
2023-02-13 15:40:29 +0900

    ➕[add]: Init E05 Files

---
+++ E05_Image_Card_State/app/src/main/java/com/cholee/e05_image_card_state/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.cholee.e05_image_card_state.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
